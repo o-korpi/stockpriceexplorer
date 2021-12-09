b79066e783d91034ba92c2087b137cb0c2c051ca
Sclip <sclip@users.noreply.github.com>
2021-12-09 20:23:55 +0100

    fixed a pretty big bug

--- src/main/kotlin/data/StockData.kt
+++ src/main/kotlin/data/StockData.kt
@@ -4,34 +4,26 @@ import java.math.BigDecimal
 import yahoofinance.YahooFinance
 
 
-fun getStockPrice(ticker: String): BigDecimal {
+fun getStockPrice(ticker: String): BigDecimal? {
     // Gets the current price (15-min delay) for a given ticker, returns -1 if the ticker is invalid
 
-    val invalidReturn = BigDecimal(-1)
-
-    if (ticker.isEmpty()) return invalidReturn
-    if (ticker.length > 10) return invalidReturn
+    if (ticker.isEmpty()) return null
+    if (ticker.length > 10) return null
 
     val price: BigDecimal = try {
         YahooFinance.get(ticker).quote.price
     } catch(e: NullPointerException) {
-        invalidReturn
+        return null
     }
 
     return price
 }
 
 
-private fun formatStockPriceData(price: BigDecimal): String {
-    // Turn invalid prices to error messages
-    // is it necessary to have 3 functions? prob not, but I did it anyways
-    if (price != BigDecimal(-1)) return "$price"
-    return "Invalid ticker"
-}
-
-
 fun getFormattedStockPrice(ticker: String): String {
     // Gets the current price (15-min delay) for a given ticker
+    val price = getStockPrice(ticker)
 
-    return formatStockPriceData(getStockPrice(ticker))
+    if (price is BigDecimal) return "$price"
+    return "Invalid ticker"
 }
